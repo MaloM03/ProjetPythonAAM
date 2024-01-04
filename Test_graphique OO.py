7a6a546e84d4a3bc7e51aa1b25478de689e3af68
asanguy <alicia.sanguy@gmail.com>
2024-01-04 15:27:27 +0100

    implementation com

--- Test_graphique OO.py
+++ Test_graphique OO.py
@@ -1,5 +1,82 @@
 import tkinter as tk
 from tkinter import ttk
+import xmlrpc.client
+
+class IF_ErpOdoo:
+    "Classe objet d'interface de l'ERP Odoo en XML-RPC"
+    def __init__(self, erp_ipaddr, erp_port, erp_db, erp_user, erp_pwd):
+        "Methode Constructeur de Classe"
+        print("##IF_ErpOdoo Constructor##")
+        self.mErpIpAddr = erp_ipaddr
+        self.mErpIpPort = erp_port
+        self.mUserId = 0
+        self.mErpDB = erp_db
+        self.mErpUser = erp_user
+        self.mErpPwd = erp_pwd
+        self.mModels = None
+    def __del__(self):
+        
+        "Methode Constructeur de Classe"
+        print("##IF_ErpOdoo Destructor##")
+
+    def connect(self):
+        "Methode de connexion à l'ERP Odoo"
+        erp_url = f'http://{self.mErpIpAddr}:{self.mErpIpPort}'
+        print("Connexion ODOO")
+        print(f"@URL={erp_url}")
+        try:
+            common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
+            version = common.version()
+            print(f"Odoo version={version['server_serie']}")
+        except ConnectionRefusedError:
+            print("Odoo Server not found or connection rejected")
+        else:
+            self.mUser_id = common.authenticate(self.mErpDB, self.mErpUser, self.mErpPwd, {})
+            print(f"Odoo authentification:{self.mUser_id}")
+        if(self.mUser_id!=False):
+            self.mModels = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(erp_url))
+            access = self.mModels.execute_kw(self.mErpDB, self.mUser_id, self.mErpPwd,
+                'mrp.production', 'check_access_rights',
+                ['write'], {'raise_exception': False})
+            print(f"Manufactoring Order write access rights : {access}")
+        else:
+            print(f'Odoo Server authentification rejected : DB={self.mErpDB} User={self.mErpUser}')
+
+    def getFields(self):
+        if(self.mModels!=None):
+            listing = self.mModels.execute_kw(self.mErpDB, self.mUser_id, self.mErpPwd,
+                'mrp.production', 'fields_get',
+                [], {'attributes': []})
+        for attr in listing:
+            print(f' - {attr}')
+
+    def getManufOrderToDo(self):
+        if(self.mModels!=None):
+            fields =['name', 'date_planned_start', 'product_id', 'product_qty', 'qty_producing', 'state']
+            limit = 10
+            global mo_list
+            mo_list = self.mModels.execute_kw(self.mErpDB, self.mUser_id, self.mErpPwd,
+                'mrp.production', 'search_read',
+                [[('state', '=', 'confirmed'), ('qty_produced', '!=', 'product_qty')]],
+                {'fields': fields, 'limit': limit})
+            for mo_dico in mo_list:
+                print(f'----------------------------')
+                for k in mo_dico.keys():
+                    print(f' - {k} : {mo_dico[k]}')
+
+    def getArticle(self):
+        if self.mModels is not None:
+            fields = ['product_id', 'qty_producing']
+            limit = 100
+            global mo_list
+            mo_list = self.mModels.execute_kw(self.mErpDB,self.mUser_id,self.mErpPwd,
+                'mrp.production','search_read',
+                [],
+                {'fields': fields, 'limit': limit})
+            for mo_dico in mo_list:
+                print(f'----------------------------')
+                for k in mo_dico.keys():
+                    print(f' - {k} : {mo_dico[k]}')
 
 #=====PAGE DE CONNECTION=====
 class Connection:
@@ -150,6 +227,12 @@ class Application :
     
 
 if __name__ == "__main__":
+
+    ifOdoo = IF_ErpOdoo("172.31.10.171", "8069","amaDB", "vente", "ventepython2024")
+    ifOdoo.connect()
+    ifOdoo.getFields()
+    ifOdoo.getManufOrderToDo()
+
     # Création de la fenêtre principale
     prod_page = tk.Tk()
     prod_page.withdraw()
@@ -163,5 +246,6 @@ if __name__ == "__main__":
 
     # Lancement de l'application
     connection_page.mainloop()
+
 
 
