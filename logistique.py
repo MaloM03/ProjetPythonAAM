9a7ba5cbadae6e1a724ca68a3de0a0327079dae2
asanguy <alicia.sanguy@gmail.com>
2024-01-18 10:37:51 +0100

    logistique qui marche

--- logistique.py
+++ logistique.py
@@ -221,14 +221,26 @@ class Application :
         # Ajout du label sous le tableau
         prod_label = tk.Label(self.logistique, text="Modifié stok")
         prod_label.grid(row=1, column=0, sticky="w", padx=5, pady=5)
+        
+        # Ajout du label erreur saisie 
+        global prod_labelerror
+        prod_labelerror = tk.Label(self.logistique, text="Saisie invalide")
+        prod_labelerror.config(fg="red")
+        prod_labelerror.grid(row=4, column=0, sticky="n", padx=5, pady=5)
+        prod_labelerror.grid_forget() 
 
         # Ajout de la zone de saisie sous le label
         self.AjoutProd = tk.Entry(self.logistique)
         self.AjoutProd.grid(row=2, column=0, sticky="nsew", padx=5, pady=5)
 
+
         # Ajout du bouton de validation
         validate_button = tk.Button(self.logistique, text="Valider", command=self.validate_entry)
         validate_button.grid(row=3, column=0, sticky="nsew", padx=5, pady=5)
+
+        # Ajout du bouton refresh
+        refresh_BP = tk.Button(self.logistique, text="refresh", command=self.refresh)
+        refresh_BP.grid(row=4, column=0, sticky="w", padx=5, pady=5)
         
      #=====AFFICHAGE DU TABLEAU DES PRODUITS===== 
     def add_data_to_table(self):
@@ -250,27 +262,41 @@ class Application :
         for item1 in data:
             self.tree.insert("", "end", values=item1)
 
+
      #=====BOUTON DE VALIDATION SAISIE STOK=====
+            
+            
     def validate_entry(self): 
-         prod_labelerror = tk.Label(self.logistique, text="Saisie invalide")
-         prod_labelerror.config(fg="red")
-         prod_labelerror.grid(row=1, column=0, sticky="n", padx=5, pady=5)
+         
          ModifStok = self.AjoutProd.get()
+    
          if ModifStok.isdigit():
          # Fonction appelée lors de la validation du bouton
-          self.add_data_to_table()
+            self.add_data_to_table()
 
          # Efface la saisie AjoutProd après la validation
-          self.AjoutProd.delete(0, 'end')
+            self.AjoutProd.delete(0, 'end')
 
-          #Efface l'erreur de saisie 
-          prod_labelerror.grid_remove() 
+         #Efface l'erreur de saisie 
+            prod_labelerror.grid_forget()
+
          else: 
-          prod_labelerror.grid()
-           
+            prod_labelerror.grid(row=4, column=0)
 
+         # Efface la saisie AjoutProd après la validation
+            self.AjoutProd.delete(0, 'end')
+    
+    def refresh(self):
+
+          # Fonction appelée lors de la validation du bouton
+            self.add_data_to_table()
+            
             # Efface la saisie AjoutProd après la validation
-          self.AjoutProd.delete(0, 'end')
+            self.AjoutProd.delete(0, 'end')
+
+         #Efface l'erreur de saisie 
+            prod_labelerror.grid_forget()
+           
              
              
 
