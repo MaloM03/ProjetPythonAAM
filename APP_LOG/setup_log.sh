aea01e454ba5ba2669b23c5a1c35d473a928b92d
asanguy <155457884+asanguy@users.noreply.github.com>
2024-03-14 17:56:20 +0100

    setup_log.sh

---
+++ APP_LOG/setup_log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+sudo apt update
+sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tgz
+tar -xf Python-3.9.2.tgz
+cd Python-3.9.2
+./configure --enable-optimizations
+make -j$(nproc)
+sudo make altinstall
+cd ..
+rm -rf Python-3.9.2 Python-3.9.2.tgz
+sudo apt-get install python3-pil.imagetk
+
+cd /home/user/Bureau/ProjetPythonAAM-main/APP_LOG
+python3 main.py
